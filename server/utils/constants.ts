1c4bbe39984a4bac428e9bf92b2f664ebb7ea740
Nguyen Son <nson.next@gmail.com>
2021-07-18 10:29:35 +0700

    change cookie config

--- server/utils/constants.ts
+++ server/utils/constants.ts
@@ -4,7 +4,7 @@ export const dev = process.env.NODE_ENV !== 'production';
 export const cookieConfig: CookieSerializeOptions = {
   httpOnly: true,
   maxAge: 60 * 60 * 24 * 365 * 10,
-  sameSite: !dev ? 'none' : 'lax',
+  sameSite: 'lax',
   secure: !dev,
   signed: true,
 };
